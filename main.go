32e4cb25f1e9b40d9114ff35d530dbd46b984f2b
tpkowastaken <protitom@gmail.com>
2024-07-09 15:57:06 +0200

    super

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	for i := 0; i < 10; i++ {
-		fmt.Printf("Jsem super preesne tolikrat: %v\n", i+1)
+		for i := 0; i < 10; i++ {
+			fmt.Printf("Jsem super asi tolikrat: %v\n", i+1)
 
-		fmt.Println("Hello, World!")
-
+		}
 	}
 
 }
