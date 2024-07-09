4b547387f18d690e87a12ed65e3efe87d98c257e
klepetowastaken <krystof.kittler@gmail.com>
2024-07-09 15:49:36 +0200

    Revert "fix"
    This reverts commit 5989a71b64c3f631af2a5de6249474dd5498fb4f.

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 
-	for i := 0; i < 10; {
-		fmt.Printf("Jsem super preesne tolikrat: %v\n", i)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Jsem super preesne tolikrat: %v\n", i+1)
 
 	}
 
