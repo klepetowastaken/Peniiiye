09620349797a012bf7bb2d16e9d47c862e9957ff
Krystof Kittler <krystofkittler@Krystof--MacBook-Air.local>
2024-09-13 22:51:49 +0200

    hellp t

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-
-	for i := 0; i < 10; i++ {
-
-		fmt.Printf("Jsem super asi tolikrat: %v\n", i+1)
-
-		fmt.Println("zdravím, World!")
-	}
+	 
 	
+
 }
